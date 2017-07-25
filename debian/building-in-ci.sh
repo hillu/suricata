c831659163afbb93ed8eaf33a93cd3a65a360e35
Arturo Borrero Gonzalez <arturo@debian.org>
2017-07-25 22:06:10 +0200

    suricata: support for internal unittest in autopktest
    Add basic support to run the internal suricata testsuite from
    debian autopkgtest.
    
    The suricata software requires a special configure-time flag to enable
    unittest/ci checks. Care should be taken to not enable this for a standard
    build, since it will surely hurt performance of suricata.
    
    In order to enable this flag, we check if we are running from
    adt/debci/autpkgtest or something like this. If that's the case, then we
    enable the configure-time flag.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo@debian.org>

---
+++ debian/building-in-ci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# this script prints 'true' if any ancestor process name is any of $REGEXPS
+
+REGEXPS="debci autopkgtest adt"
+
+walk()
+{
+	pid=$1
+
+	[ ! -r /proc/$pid/cmdline ] && exit 1
+
+	name=$(ps -p $pid -o cmd | tail -1)
+	for exp in $REGEXPS
+	do
+		if grep -e $exp <<< $name >/dev/null ; then
+			echo true
+			exit
+		fi
+	done
+
+	ppid=$(ps -o ppid= $pid | tr -d ' ')
+	walk $ppid
+}
+
+walk $$
